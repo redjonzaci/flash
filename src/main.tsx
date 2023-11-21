4a968e5623f19c980f499221a9fa9e0c309f8b45
redjonzaci <redjonzaci@gmail.com>
2023-11-21 14:15:29 +0100

    chore: install and configure Amplify

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
+import { Amplify } from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import awsmobile from './aws-exports';
 import './index.css';
+
+Amplify.configure(awsmobile);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
