98810ada6a7b940a0e6214d148faa67d2d461409
redjonzaci <redjonzaci@gmail.com>
2023-11-22 18:42:52 +0100

    feat: filter by category

--- src/components/TicketBoard.tsx
+++ src/components/TicketBoard.tsx
@@ -1,11 +1,13 @@
 import { DataStore, SortDirection } from 'aws-amplify/datastore';
 import { useEffect, useState } from 'react';
-import { Ticket } from '../models';
+import { Category, Ticket } from '../models';
+import './button.css';
 import './link.css';
 import './table.css';
 
 export default function TicketBoard() {
   const [tickets, setTickets] = useState<Ticket[]>([]);
+  const [filteredTickets, setFilteredTickets] = useState<Ticket[]>([]);
 
   useEffect(() => {
     DataStore.query(Ticket, (t) => t.category.ne(Category.NOT_AN_ISSUE), {
@@ -13,15 +15,19 @@ export default function TicketBoard() {
       sort: (ticket) => ticket.timestamp(SortDirection.DESCENDING),
     }).then((tickets) => {
       setTickets(tickets);
+      setFilteredTickets(tickets);
     });
   }, []);
 
   useEffect(() => {
     const subscription = DataStore.observe(Ticket).subscribe(() => {
-      DataStore.query(Ticket, (t) => t.category.eq(Category.NOT_AN_ISSUE), {
+      DataStore.query(Ticket, (t) => t.category.ne(Category.NOT_AN_ISSUE), {
         limit: 20,
         sort: (ticket) => ticket.timestamp(SortDirection.DESCENDING),
-      }).then(setTickets);
+      }).then((tickets) => {
+        setTickets(tickets);
+        setFilteredTickets(tickets);
+      });
     });
 
     return () => subscription.unsubscribe();
@@ -68,44 +74,72 @@ export default function TicketBoard() {
       setHasMore(false);
     } else {
       setTickets([...tickets, ...newTickets]);
+      setFilteredTickets([...tickets, ...newTickets]);
+    }
+  }
+
+  function filterTicketsByCategory(category: string) {
+    if (category === 'All') {
+      setFilteredTickets(tickets);
+    } else {
+      DataStore.query(Ticket, (ticket) =>
+        ticket.category.eq(category as Category)
+      ).then(setFilteredTickets);
     }
   }
 
   return (
-    <table className="tds-table tds-o-table-cells-centered tds-table--thead_border tds-table--padded_cells tds-table--hoverable tds-table--row_border tds-o-legacy-careers">
-      <thead className="tds-table-head">
-        <tr className="tds-table-row">
-          <th role="columnheader" scope="col">
-            Title
-          </th>
-          <th role="columnheader" scope="col">
-            Category
-          </th>
-          <th role="columnheader" scope="col">
-            Content
-          </th>
-          <th role="columnheader" scope="col">
-            Timestamp
-          </th>
-        </tr>
-      </thead>
-      <tbody className="tds-table-body">
-        {tickets.map((ticket) => (
-          <tr className="tds-table-row" key={ticket.id}>
-            <td>
-              <a href={ticket.url} className="tds-link" target="_blank">
-                {ticket.title} (
-                {ticket.sourceType[0] +
-                  ticket.sourceType.toLowerCase().slice(1)}
-                )
-              </a>
-            </td>
-            <td>{ticket.category || ''}</td>
-            <td>{ticket.content.slice(0, 200)}...</td>
-            <td>{new Date(ticket.timestamp).toLocaleString()}</td>
-          </tr>
+    <>
+      <div>
+        {[
+          'All',
+          ...Object.values(Category).filter((c) => c !== Category.NOT_AN_ISSUE),
+        ].map((category) => (
+          <button
+            key={category}
+            onClick={() => filterTicketsByCategory(category)}
+            className="tds-btn tds-btn--secondary"
+            style={{ marginRight: '0.5rem' }}
+          >
+            {category}
+          </button>
         ))}
-      </tbody>
-    </table>
+      </div>
+      <table className="tds-table tds-o-table-cells-centered tds-table--thead_border tds-table--padded_cells tds-table--hoverable tds-table--row_border tds-o-legacy-careers">
+        <thead className="tds-table-head">
+          <tr className="tds-table-row">
+            <th role="columnheader" scope="col">
+              Title
+            </th>
+            <th role="columnheader" scope="col">
+              Category
+            </th>
+            <th role="columnheader" scope="col">
+              Content
+            </th>
+            <th role="columnheader" scope="col">
+              Timestamp
+            </th>
+          </tr>
+        </thead>
+        <tbody className="tds-table-body">
+          {filteredTickets.map((ticket) => (
+            <tr className="tds-table-row" key={ticket.id}>
+              <td>
+                <a href={ticket.url} className="tds-link" target="_blank">
+                  {ticket.title} (
+                  {ticket.sourceType[0] +
+                    ticket.sourceType.toLowerCase().slice(1)}
+                  )
+                </a>
+              </td>
+              <td>{ticket.category || ''}</td>
+              <td>{ticket.content.slice(0, 200)}...</td>
+              <td>{new Date(ticket.timestamp).toLocaleString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 }
